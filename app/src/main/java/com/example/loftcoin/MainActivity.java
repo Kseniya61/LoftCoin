642d39aa3484071f3343389c3e70c171091486d7
Kseniya <kseniya.karagodina2005@gmail.com>
2020-04-05 17:23:22 +0600

    Add adaptive icons

--- app/src/main/java/com/example/loftcoin/MainActivity.java
+++ app/src/main/java/com/example/loftcoin/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.loftcoin;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toast.makeText(this, "111", Toast.LENGTH_LONG).show();
     }
 }
